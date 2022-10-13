41e8afb07a459d5810a3c4ef36db1e6b4dac5841
BharpreetSingh <b00139858@mytudublin.ie>
2022-10-13 21:54:50 +0100

    Added WebGL

---
+++ Challenge 2/Assets/link.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class link : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://github.com/BharpreetSingh/Challenge2-Create-With-Code.git");
+    }
+}
+
